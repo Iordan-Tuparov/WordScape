20bf112886a0e7c3bc45509ec6984fa659b21042
Iordan Tuparov <161603990+Iordan-Tuparov@users.noreply.github.com>
2024-08-10 00:42:27 +0300

    add font awesome icons

--- client/src/components/Footer/FooterComponent.jsx
+++ client/src/components/Footer/FooterComponent.jsx
@@ -1,17 +1,19 @@
 import './Footer.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFaceGrin } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faInstagram, faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 
 function FooterComponent() {
     return (
         <>
-            <footer>Site designed by Iordan &copy;
+            <footer>
                 <ul>
-                    <li href="#"><FontAwesomeIcon icon={faFaceGrin} /></li>
-                    <li href="#">Link</li>
-                    <li href="#">Link</li>
-                    <li href="#">Link</li>
+                    <p>&copy; 2024 WordScape</p>
+                    <p>Designed by Iordan Typarow</p>
+                    <li><a href="https://www.facebook.com/profile.php?id=100084871037402"><FontAwesomeIcon icon={faFacebook} /></a></li>
+                    <li><a href="https://www.instagram.com/_tuparow_"><FontAwesomeIcon icon={faInstagram} /></a></li>
+                    <li><a href="https://www.linkedin.com/in/iordan-typarov-5848b3299"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+                    <li><a href="https://github.com/Iordan-Tuparov"><FontAwesomeIcon icon={faGithub} /></a></li>
                 </ul>
             </footer>
 
